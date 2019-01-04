0412fbd8bc128e10663a0399684c6bdcf75c5613
gallib2 <gallib2@gmail.com>
2019-01-04 10:47:03 +0200

    destroyLife

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -8,11 +8,30 @@ public class Spaceship : MonoBehaviour {
 
     public GameObject theBullet;
 
+    public Transform initialPosition;
+
+    private void Start()
+    {
+        initialPosition = gameObject.transform;
+    }
+
     private void FixedUpdate()
     {
         float horzMove = Input.GetAxisRaw("Horizontal");
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(horzMove, 0) * speed;
+
+
+    }
+
+    private void OnDestroy()
+    {
+        GameObject life = GameObject.FindGameObjectWithTag("Life");
+
+        if(life != null)
+        {
+            Destroy(life);
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/SpaceshipController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipController : MonoBehaviour {
+
+    public Spaceship spaceship;
+
+    private void Start()
+    {
+       // spaceship = GetComponent<Spaceship>();
+    }
+
+    private void Update()
+    {
+        Debug.Log("spaceship: ");
+        Debug.Log(spaceship.gameObject);
+        if(spaceship == null)
+        {
+            OnSpaceshipDestroy();
+        }
+    }
+
+    private void OnSpaceshipDestroy()
+    {
+        Debug.Log("here in spaceship conrteoller");
+        Instantiate(spaceship, spaceship.initialPosition);
+    }
+}
